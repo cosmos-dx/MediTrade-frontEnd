0a782b4a2ff53d9bf7e4ce778855c9e9a098d4ec
Abhishek <abhishekgupta0118@gmail.com>
2023-05-09 02:31:34 +0530

    updated navbar

--- model/medifun.js
+++ model/medifun.js
@@ -1,12 +1,3 @@
-
-
-
-const memdb = require(Path.join(Path.dirname(__dirname),"rmsconfig//member"));
-var db = null; //require(Path.join(Path.dirname(__dirname),"rmsconfig\\rmsdb"));
-//const memdb = require('nod/rmsconfig/member');
-//const db = require('./rmsconfig/rmsdb');
-
-
 function emptyRSCR(){
     var rscr = {"info":"MediTrade-Soft", "alert":"","title":"","htmlheaderdp":"","pathinfo":"",
         "updatemediregister":false, "mediregister":"","getsdc":"", "csinfo":"","cinfo":"","sinfo":"",
@@ -88,7 +79,4 @@ function readRSCR(){
 
 module.exports.emptyRSCR = emptyRSCR
 module.exports.readRSCR = readRSCR
-
-module.exports.GetUserDirName = GetUserDirName
-module.exports.dbfileCreate = dbfileCreate
 
